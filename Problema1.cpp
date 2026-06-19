bddc68e8db787991217cfaf48e64a7b923f52736
Giovannym27 <gimamanim@unjbg.edu.pe>
2026-06-18 21:28:52 -0500

    EjercicioA: Integracion de las estructuras for para culminar el codigo

--- Problema1.cpp
+++ Problema1.cpp
@@ -4,6 +4,17 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
+    
+    int arreglo[N];
 
+    for (int i = 0; i < N; i++) {
+        cin >> arreglo[i];
+    }
+
+    for (int i = N - 1; i >= 0; i--) {
+        cout << arreglo[i] << " ";
+    }
+
+    return 0;
 
 }
