d53ca35e5c8937e2d55169841d8657e84aadfc2b
Giovannym27 <gimamanim@unjbg.edu.pe>
2026-06-20 19:31:15 -0500

    EjercicioB: Primer intento de Palíndromos Coding Rush

---
+++ Problema2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    if (cin >> n) {
+        for (int i = 0; i < n; i++) {
+            string palabra;
+            cin >> palabra;
+            
+            
+            
+        }
+    }
+    
+    return 0;
+}
