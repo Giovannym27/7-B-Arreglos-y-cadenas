c46bc2f2b58d8b6bc2b841397ca90a92fc7c1d24
Giovannym27 <gimamanim@unjbg.edu.pe>
2026-06-20 19:56:23 -0500

    EjercicoC: Implementacion del arreglo y las estructuras for

--- Problema3.cpp
+++ Problema3.cpp
@@ -5,8 +5,17 @@ using namespace std;
 int main() {
     int p, n;
     if (cin >> p >> n) {
+        int frecuencias[105] = {0};
         
-
+        for (int i = 0; i < n; i++) {
+            int voto;
+            cin >> voto;
+            frecuencias[voto]++;
+        }
+        
+        for (int i = 1; i <= p; i++) {
+            cout << i << "-" << frecuencias[i] << endl;
+        }
     }
     
     return 0;
