9c348d25a4d214e2cc7629647b3563d9b867b2de
Giovannym27 <gimamanim@unjbg.edu.pe>
2026-06-21 21:42:39 -0500

    Implementacion de las estructuras if y for para la culminacion de codigo

--- problema4.cpp
+++ problema4.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main() {
     int n, c;
-
+    if (cin >> n >> c) {
+        int vasos[1005] = {0};
+        
+        for (int i = 0; i < c; i++) {
+            int posicion;
+            cin >> posicion;
+            vasos[posicion]++;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            cout << vasos[i] << endl;
+        }
+    }
     return 0;
 }
