132d78a204c2d81d4b00e7da56b1ed6cb979b16a
Giovannym27 <gimamanim@unjbg.edu.pe>
2026-06-23 11:47:25 -0500

    EjercicioF: Primer intento Ordenando las letras de la linea

---
+++ problema5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string linea;
+    if (getline(cin, linea)) {
+
+    }
+    
+    return 0;
+}
