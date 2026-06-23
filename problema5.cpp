e2cf10b796a17ffdbc98014a7ebaee46ea23608b
Giovannym27 <gimamanim@unjbg.edu.pe>
2026-06-23 11:51:10 -0500

    EjercicioF: Implementacion de la estructuras for, if y el puntero logico

--- problema5.cpp
+++ problema5.cpp
@@ -7,7 +7,25 @@ using namespace std;
 int main() {
     string linea;
     if (getline(cin, linea)) {
-
+        string letras = "";
+        
+        for (int i = 0; i < linea.length(); i++) {
+            if (linea[i] >= 'a' && linea[i] <= 'z') {
+                letras += linea[i];
+            }
+        }
+        
+        sort(letras.begin(), letras.end());
+        
+        int indiceLetras = 0;
+        for (int i = 0; i < linea.length(); i++) {
+            if (linea[i] >= 'a' && linea[i] <= 'z') {
+                linea[i] = letras[indiceLetras];
+                indiceLetras++;
+            }
+        }
+        
+        cout << linea << endl;
     }
     
     return 0;
